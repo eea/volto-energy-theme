a4a447bfed1b6ef55d699f3620da3d0e667c11d8
nileshgulia1 <nileshgulia@gmail.com>
2022-05-17 15:24:34 +0530

    shift config to volto-energy-policy

--- src/index.js
+++ src/index.js
@@ -41,33 +41,6 @@ import '@plone/volto/config';
 export default function applyConfig(config) {
   // Add here your project's configuration here by modifying `config` accordingly
 
-  const allowed_cors_destinations = [
-    ...(config.settings.allowed_cors_destinations || []),
-    'www.eea.europa.eu',
-    'www.eionet.europa.eu',
-    'land.copernicus.eu',
-  ];
-
-  config.settings = {
-    ...config.settings,
-    apiExpanders: [
-      ...config.settings.apiExpanders,
-      {
-        match: '/',
-        GET_CONTENT: [
-          'breadcrumbs',
-          'siblings',
-          'navigation',
-          'localnavigation',
-        ],
-      },
-    ],
-    navDepth: 4,
-    allowed_cors_destinations,
-    tableauVersion: '2.3.0',
-    pdfWorkerSrc: '//www.eea.europa.eu/pdfjs/pdf.worker.min.js',
-  };
-
   config.views = {
     ...config.views,
     layoutViews: {
